9da29872e0e6fb60661cec6e0381dfc6710b017c
ko-kamenashi <kamenashi0917@gmail.com>
2021-01-28 20:09:58 +0900

    feat: created search feature
    pushed under src dir by @google/clasp from local env

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-function myFunction() {
+function searchBoxDocs(): void {
+  const form = FormApp.getActiveForm();
+  const items = form.getItems();
+
+  items.forEach(item => {
+    switch (item.getType()) {
+      case FormApp.ItemType.CHECKBOX:
+        const choices = item.asCheckboxItem().getChoices();
+
+        choices.forEach(choice => Logger.log(choice.getValue()));
+        break;
+
+      default:
+        break;
+    }
+  });
+
+  doGet();
   Logger.log("Success!!");
 }
